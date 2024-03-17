452ff4e4f60af8b0b8c60a9cf2bbbab6c563656e
shejialuo <shejialuo@gmail.com>
2024-03-17 23:00:22 +0800

    Finish Chapter 11

--- src/Chapter11/Exercise.hs
+++ src/Chapter11/Exercise.hs
@@ -1,6 +1,7 @@
 module Chapter11.Exercise where
 
 import Chapter9.Exercise (capitalizeFirstLetter)
+import Data.Char (toUpper)
 
 isSubseqOf :: (Eq a) => [a] -> [a] -> Bool
 isSubseqOf [] _ = True
@@ -16,4 +17,22 @@ capitalizeWord :: String -> String
 capitalizeWord = capitalizeFirstLetter
 
 capitalizeParagraph :: String -> String
-capitalizeParagraph = undefined
+capitalizeParagraph = helper True
+  where
+    helper _ [] = []
+    helper _ ('.' : xs) = '.' : helper True xs
+    helper state (' ' : xs) = ' ' : helper state xs
+    helper True (x : xs) = toUpper x : helper False xs
+    helper False (x : xs) = x : helper False xs
+
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit x) = x
+eval (Add e1 e2) = eval e1 + eval e2
+
+printExpr :: Expr -> String
+printExpr (Lit x) = show x
+printExpr (Add e1 e2) = printExpr e1 ++ " + " ++ printExpr e2
--- test/Chapter11/ExerciseTest.hs
+++ test/Chapter11/ExerciseTest.hs
@@ -1,6 +1,6 @@
 module Chapter11.ExerciseTest where
 
-import Chapter11.Exercise (capitalizeWord, capitalizeWords, isSubseqOf)
+import Chapter11.Exercise (Expr (Add, Lit), capitalizeParagraph, capitalizeWord, capitalizeWords, eval, isSubseqOf, printExpr)
 import Test.HUnit (Test (TestCase, TestList), assertEqual)
 
 testIsSubseqOf :: Test
@@ -49,10 +49,72 @@ testCapitalizeWord =
           $ capitalizeWord "Hello"
     ]
 
+testCapitalizeParagraph :: Test
+testCapitalizeParagraph =
+  TestList
+    [ TestCase $ do
+        assertEqual
+          "capitalizeParagraph \"blah. woot ha.\""
+          "Blah. Woot ha."
+          $ capitalizeParagraph "blah. woot ha."
+        -- Generate more tests
+        assertEqual
+          "capitalizeParagraph \"blah. woot ha. blah. woot ha.\""
+          "Blah. Woot ha. Blah. Woot ha."
+          $ capitalizeParagraph "blah. woot ha. blah. woot ha."
+    ]
+
+testEval :: Test
+testEval =
+  TestList
+    [ TestCase $ do
+        assertEqual
+          "eval (Add (Lit 1) (Lit 9001))"
+          9002
+          $ eval (Add (Lit 1) (Lit 9001))
+        assertEqual
+          "eval (Add (Lit 1) (Add (Lit 9001) (Lit 1)))"
+          9003
+          $ eval (Add (Lit 1) (Add (Lit 9001) (Lit 1)))
+        assertEqual
+          "eval (Add (Lit 1) (Add (Lit 9001) (Add (Lit 1) (Lit 1))))"
+          9004
+          $ eval (Add (Lit 1) (Add (Lit 9001) (Add (Lit 1) (Lit 1))))
+    ]
+
+testPrintEval :: Test
+testPrintEval =
+  TestList
+    [ TestCase $ do
+        assertEqual
+          "printExpr (Add (Lit 1) (Lit 9001))"
+          "1 + 9001"
+          $ printExpr (Add (Lit 1) (Lit 9001))
+        assertEqual
+          "printExpr (Add (Lit 1) (Add (Lit 9001) (Lit 1)))"
+          "1 + 9001 + 1"
+          $ printExpr (Add (Lit 1) (Add (Lit 9001) (Lit 1)))
+        assertEqual
+          "printExpr (Add (Lit 1) (Add (Lit 9001) (Add (Lit 1) (Lit 1))))"
+          "1 + 9001 + 1 + 1"
+          $ printExpr (Add (Lit 1) (Add (Lit 9001) (Add (Lit 1) (Lit 1))))
+        assertEqual
+          "printExpr (Add (Add (Lit 1) (Lit 9001)) (Add (Lit 1) (Lit 1)))"
+          "1 + 9001 + 1 + 1"
+          $ printExpr (Add (Add (Lit 1) (Lit 9001)) (Add (Lit 1) (Lit 1)))
+        assertEqual
+          "printExpr (Add (Add (Lit 1) (Lit 9001)) (Add (Lit 1) (Add (Lit 1) (Lit 1))))"
+          "1 + 9001 + 1 + 1 + 1"
+          $ printExpr (Add (Add (Lit 1) (Lit 9001)) (Add (Lit 1) (Add (Lit 1) (Lit 1))))
+    ]
+
 testSuite :: Test
 testSuite =
   TestList
     [ testIsSubseqOf,
       testCapitalizeWords,
-      testCapitalizeWord
+      testCapitalizeWord,
+      testCapitalizeParagraph,
+      testEval,
+      testPrintEval
     ]
